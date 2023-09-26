30a8dc5d002538df12d6f82c8fdfe482923c41fc
Ben Ray <70728918+BenRay2024@users.noreply.github.com>
2023-09-25 18:28:13 -0700

    Delete data-labeling directory

--- data-labeling/docker-shell.bat
+++
@@ -1,9 +0,0 @@
-
-REM Create the network if we don't have it yet
-docker network inspect data-labeling >/dev/null 2>&1 || docker network create data-labeling
-
-REM Build the image based on the Dockerfile
-docker build -t data-label-cli -f Dockerfile .
-
-REM Run All Containers
-docker-compose run --rm --service-ports data-label-cli
