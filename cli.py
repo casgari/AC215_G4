30c388c975c392384794cb4e771c8d17d6520691
Ben Ray <benray@college.harvard.edu>
2023-10-06 19:13:44 -0400

    Delete clutter (empty folders and references/tutorials)

--- cli.py
+++ cli.py
@@ -41,33 +41,6 @@ def download_data():
             local_file_path = os.path.join(dataset_folder, filename)
             blob.download_to_filename(local_file_path)
 
-    # # Organize annotation with images
-    # annotation_files = glob.glob(os.path.join(dataset_prep_folder, "*"))
-    # for annotation_file in annotation_files:
-    #     # Read the json file
-    #     with open(annotation_file, "r") as read_file:
-    #         annotation_json = json.load(read_file)
-
-    #     # Annotations
-    #     # annotations = annotation_json["annotations"]
-    #     # Assume we pick just the first annotation from the labeled list
-    #     if len(annotation_json["result"]) > 0:
-    #         label = annotation_json["result"][0]["value"]["choices"][0]
-    #         # Create the label folder
-    #         label_folder = os.path.join(dataset_folder, label)
-    #         os.makedirs(label_folder, exist_ok=True)
-
-    #         # Download the image from GCS [Another option could be to just store the image url and label in DVC]
-    #         image_url = annotation_json["task"]["data"]["image"]
-    #         image_url = image_url.replace("gs://", "").replace(
-    #             GCS_BUCKET_NAME + "/", ""
-    #         )
-    #         print("image_url:", image_url)
-    #         blob = bucket.blob(image_url)
-    #         filename = os.path.basename(blob.name)
-    #         local_file_path = os.path.join(label_folder, filename)
-    #         blob.download_to_filename(local_file_path)
-
 
 def main(args=None):
     if args.download:
--- test_project.py
+++
@@ -1,2 +0,0 @@
-def test_pytest_installed():
-    assert "a" == "a"
