19116d7578f3678e7457c3bfe5d9c3021c518fda
CalebSaul <110218739+CalebSaul@users.noreply.github.com>
2023-11-13 18:50:58 -0500

    tutorial 2

--- src/api-service/docker-shell.sh
+++ src/api-service/docker-shell.sh
@@ -8,9 +8,12 @@ export IMAGE_NAME="mushroom-app-api-service"
 export BASE_DIR=$(pwd)
 export SECRETS_DIR=$(pwd)/../../../secrets/
 export PERSISTENT_DIR=$(pwd)/../../../persistent-folder/
+export GCS_BUCKET_NAME="mushroom-app-models"
 
 # Build the image based on the Dockerfile
-docker build -t $IMAGE_NAME -f Dockerfile .
+#docker build -t $IMAGE_NAME -f Dockerfile .
+# M1/2 chip macs use this line
+docker build -t $IMAGE_NAME --platform=linux/arm64/v8 -f Dockerfile .
 
 # Run the container
 docker run --rm --name $IMAGE_NAME -ti \
@@ -18,4 +21,7 @@ docker run --rm --name $IMAGE_NAME -ti \
 -v "$SECRETS_DIR":/secrets \
 -v "$PERSISTENT_DIR":/persistent \
 -p 9000:9000 \
+-e DEV=1 \
+-e GOOGLE_APPLICATION_CREDENTIALS=/secrets/ml-workflow.json \
+-e GCS_BUCKET_NAME=$GCS_BUCKET_NAME \
 $IMAGE_NAME
