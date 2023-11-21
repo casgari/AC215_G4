fa373f559c356a0b5fd264088458f54aa25424a1
Warren Sunada-Wong <warren.sunada.wong@gmail.com>
2023-11-21 17:47:20 -0500

    change bucket name

--- src/api-service/docker-shell.sh
+++ src/api-service/docker-shell.sh
@@ -8,7 +8,7 @@ export IMAGE_NAME="ppp-app-api-service"
 export BASE_DIR=$(pwd)
 export SECRETS_DIR=$(pwd)/../../secrets/
 export PERSISTENT_DIR=$(pwd)/../../persistent-folder/
-export GCS_BUCKET_NAME="ppp-app-models"
+export GCS_BUCKET_NAME="mega-ppp-ml-workflow"
 
 # Build the image based on the Dockerfile
 #docker build -t $IMAGE_NAME -f Dockerfile .
